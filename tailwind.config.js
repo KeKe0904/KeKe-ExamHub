36a8fa095feae2d626ae5b64cd9d83219a0cd135
KeKe0904 <fcattles@qq.com>
2026-06-27 07:07:43 +0800

    docs: 统一 Trae IDE 开发声明为'完全由 Trae IDE 开发'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
  * KeKe ExamHub - 考试信息管理系统
  * @author 落梦陳 (KeKe0904) | B站/抖音: 落梦陳
  * @github https://github.com/KeKe0904/KeKe-ExamHub
- * 本项目使用 Trae IDE 辅助开发生成
+ * 本项目完全由 Trae IDE 开发
  * @license MIT
  * @type {import('tailwindcss').Config}
  */
