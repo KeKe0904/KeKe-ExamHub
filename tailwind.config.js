f7a94ff1fd83967cd55512bf1f7737cbbc29ce14
KeKe0904 <fcattles@qq.com>
2026-06-27 07:05:05 +0800

    chore: 清理自动生成标识, 添加 Trae IDE 生产声明

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-
+/**
+ * KeKe ExamHub - 考试信息管理系统
+ * @author 落梦陳 (KeKe0904) | B站/抖音: 落梦陳
+ * @github https://github.com/KeKe0904/KeKe-ExamHub
+ * 本项目使用 Trae IDE 辅助开发生成
+ * @license MIT
+ * @type {import('tailwindcss').Config}
+ */
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
